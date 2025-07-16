a3249511e192818ebf171029713e42eac9fd0a07
Ankit kumar malik <ankit2102malik@gmail.com>
2025-07-16 23:22:34 +0530

    added some stream file

--- src/Collection_framework/stream/CollectorsDemo.java
+++ src/Collection_framework/stream/CollectorsDemo.java
@@ -1,43 +1,39 @@
 package Collection_framework.stream;
 
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class CollectorsDemo {
     public static void main(String[] args) {
-        //Collectors is a utility class
+
+        //Collectors id a utility class
         //provides a set of methods to create common collectors
 
         //1. Collecting to a list
         List<String> names = Arrays.asList("Alice","Bob","Charlie");
         List<String> res = names.stream()
-                .filter(name ->name.startsWith("A"))
+                .filter(name->name.startsWith("A"))
                 .collect(Collectors.toList());
 
         System.out.println(res);
 
-        //Collecting to a set
-        List<Integer> nums = Arrays.asList(1,2,2,3,4,4,5);
+        //2 collecting to a set
+        List<Integer>nums = Arrays.asList(1,2,2,3,4,4,6);
         Set<Integer> set = nums.stream().collect(Collectors.toSet());
         System.out.println(set);
 
-        // 3. Collecting to a Specific collection
+        // 3. Collectiong to a specific collection
         ArrayDeque<String> collect = names.stream().collect(Collectors.toCollection(()-> new ArrayDeque<>()));
 
-
-        // 4. Joining String
-        // Concatenates stream element into a single String
-
+        // 4.Joining Strings
+        //   Concatenated stream elements into single String
         String concatenatedNames = names.stream().map(String::toUpperCase).collect(Collectors.joining(", "));
         System.out.println(concatenatedNames);
 
-        /*
-         5) Summarizing Date
-         Generated statistical summary (count, sum, min, average, max)
-         .collect() is a terminal operation in Java 8's Stream API that
-         is used to transform the stream elements into a different data structure or a result.
-
-         */
+        // 5. Summarizing data
+        // Generates statistical summary (count,sum,min,average,max)
 
         List<Integer> numbers = Arrays.asList(2,3,5,7,11);
         IntSummaryStatistics stats = numbers.stream().collect(Collectors.summarizingInt(x->x));
@@ -47,63 +43,61 @@ public class CollectorsDemo {
         System.out.println("Average: " + stats.getAverage());
         System.out.println("Max: " + stats.getMax());
 
-        // 6. Calculating Averages
+        // 6. calculating Averages
         Double average = numbers.stream().collect(Collectors.averagingInt(x->x));
         System.out.println("Average: " + average);
 
         // 7. Counting Elements
         Long count = numbers.stream().collect(Collectors.counting());
-        System.out.println("Count: " + count);
+        System.out.println("Count: "+count);
 
-        // 8. Grouping Elements
-        List<String> words = Arrays.asList("hello","world","java","streams","collecting");
+        //8 Grouping Elements
+        List<String>words = Arrays.asList("hello","world","java","streams");
         System.out.println(words.stream().collect(Collectors.groupingBy(String::length)));
-        System.out.println(words.stream().collect(Collectors.groupingBy(String::length,Collectors.joining(", "))));
+        System.out.println(words.stream().collect(Collectors.
+                groupingBy(String::length,Collectors.joining(", "))));
         System.out.println(words.stream().collect(Collectors.groupingBy(String::length,Collectors.counting())));
         TreeMap<Integer,Long> treeMap = words.stream().collect(Collectors.groupingBy(String::length,TreeMap::new,Collectors.counting()));
-
         System.out.println(treeMap);
 
-        // 9. Partitioning Elements
-        //  Partition elements into two groups (true and false) based on a predicate
+        //9 partitioning elements
+        //partition elements into two grroup (true and false) based on a predicate
         System.out.println(words.stream().collect(Collectors.partitioningBy(x->x.length()>5)));
 
         // 10. Mapping and Collecting
         // Applies a mapping function before collecting
-        System.out.println(words.stream().collect(Collectors.mapping(x -> x.toUpperCase(),Collectors.toList())));
+        System.out.println(words.stream().collect(Collectors.mapping(x->x.toUpperCase(),Collectors.toList())));
 
         // 11. toMap
-        // Example 1: Collecting Names by Length
-        List<String> l1 = Arrays.asList("Anna","Bob","Alexander","Brian","Alice");
+
+        // Example 1:Collecting Names by Length
+        List<String> l1 = Arrays.asList("Anna","Bob","Alexander","Brain","Alice");
         System.out.println(l1.stream().collect(Collectors.groupingBy(String::length)));
 
-        // Example 3: Partitioning Even and Odd Number
+        // Example 2:Counting word Occurences
+        String sentence = "hello world hello java world";
+        System.out.println(Arrays.stream(sentence.split(" ")).collect(Collectors.groupingBy(x->x,Collectors.counting())));
+
+        // Example 3:Partitioning Even and Odd Numbers
         List<Integer> l2 = Arrays.asList(1,2,3,4,5,6);
         System.out.println(l2.stream().collect(Collectors.partitioningBy(x->x%2==0)));
 
-        // Example 4: Summing Values in a Map
+        // Example 4: Summing Value in a Map
         Map<String,Integer> items = new HashMap<>();
         items.put("Apple",10);
         items.put("Banana",20);
-        items.put("Cherry",30);
-        items.put("Mango",40);
-        items.put("Orange",50);
+        items.put("Orange",15);
 
         System.out.println(items.values().stream().reduce(Integer::sum));
         System.out.println(items.values().stream().collect(Collectors.summarizingInt(x->x)));
 
-
-        //Example 5:Creating a Map from Stream Elements
-        List<String> fruits = Arrays.asList("Apple","Banana","Cherry");
+        //Example 5: Creating a Map from Stream Elements
+        List<String> fruits = Arrays.asList("Aple","Banana","Cherry");
         System.out.println(fruits.stream().collect(Collectors.toMap(x->x.toUpperCase(),x->x.length())));
 
         // Example 6:
-        List<String> word2 = Arrays.asList("apple", "banana", "apple", "orange", "banana", "apple");
-        System.out.println(word2.stream().collect(Collectors.toMap(k->k,v->1,(x,y)->x+y)));
-
-
-
-
+        List<String> words2 = Arrays.asList("apple","banana","apple","orange","orange","banana","apple");
+        System.out.println(words2.stream().collect(Collectors.toMap(k->k,v->1,(x,y)->x+y)));
 
 
 
---
+++ src/Collection_framework/stream/IntermediateOps.java
@@ -0,0 +1,15 @@
+package Collection_framework.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntermediateOps{
+    public static void main(String[] args) {
+        // Intermediate operations transform a stream into another stream
+        // They are lazy, meaning they don't execute until a terminal operation is invoked.
+
+        // 1.filter
+        List<String> list = Arrays.asList("Akshit","Ram","Shyam","Ghanshya","Akshit");
+
+    }
+}
